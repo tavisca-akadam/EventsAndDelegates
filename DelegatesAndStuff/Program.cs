03502a2fdc333d674a683b0720531d0987b1de8a
tavisca-akadam <ankadam@tavisca.com>
2019-09-02 23:06:51 +0530

    used Ananomous function and remove delMethod
    Remove DelMethod coz remove unneccesary code

--- DelegatesAndStuff/Program.cs
+++ DelegatesAndStuff/Program.cs
@@ -8,11 +8,42 @@ namespace DelegatesAndStuff
         {
             Console.WriteLine("Hello World!");
             Worker worker = new Worker();
-            worker.WorkPerformed += DelMethod.WorkPerformed;
-            worker.WorkCompleted += DelMethod.WorkDone;
 
+            //Using Tradiition Way
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
             worker.DoWork(10, WorkType.GoToMeeting);
+            worker.WorkPerformed -= Worker_WorkPerformed;                               //TO remove the event from invocation list
+            worker.WorkPerformed -= Worker_WorkCompleted;
+
+            //Using Delegate Inference 
+            worker.WorkPerformed += Worker_WorkPerformed;
+            worker.WorkCompleted += Worker_WorkCompleted;
+            worker.DoWork(8, WorkType.GenerateReport);
+            worker.WorkPerformed -= Worker_WorkPerformed;                              //TO remove the event from invocation list
+            worker.WorkPerformed -= Worker_WorkCompleted;
+
+            //Using Ananumous Method
+            worker.WorkPerformed += delegate(object sender, WorkPerformedEventArgs e) {
+                Console.WriteLine($"number of hours {e.Hours}, by work {e.WorkType} and owner was {sender.ToString()}");
+            };
+
+            worker.WorkCompleted += delegate(object sender, EventArgs e) {
+                Console.WriteLine("Work Done");
+            };
+            worker.DoWork(6, WorkType.PlayCricket);
+
             Console.ReadKey(true);
+        }
+
+        private static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work done");
+        }
+
+        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine($"number of hours {e.Hours}, by work {e.WorkType} and owner was {sender.ToString()}");
         }
 
         public enum WorkType
